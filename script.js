ab1962e0a6abc64b6e9599973fe6605e6907983f
rahul kumar <156586219+Codesoftp28@users.noreply.github.com>
2024-01-18 22:20:24 +0530

    Add required files and some basic code

---
+++ script.js
@@ -0,0 +1,41 @@
+let question=[
+    {
+        'quest': "which of the following is markep language",
+        'a':"HTML",
+        'b':"CSS",
+        'c':"JS",
+        'd':"PHP",
+        'corrent':"a"
+    },
+    {
+        'quest': "which of the following is markep language",
+        'a':"HTML",
+        'b':"CSS",
+        'c':"JS",
+        'd':"PHP",
+        'corrent':"a"
+    },
+    {
+        'quest': "which of the following is markep language",
+        'a':"HTML",
+        'b':"CSS",
+        'c':"JS",
+        'd':"PHP",
+        'corrent':"a"
+    },
+    {
+        'quest': "which of the following is markep language",
+        'a':"HTML",
+        'b':"CSS",
+        'c':"JS",
+        'd':"PHP",
+        'corrent':"a"
+    }
+];
+let btn=document.querySelector("button");
+btn.addEventListener("click",(event)=>{
+    let 
+})
+
+
+
